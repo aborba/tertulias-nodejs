6c44fab8438bd6c817302960a57440f1c2a5c089
Antonio Borba da Silva <abs@ggl.pt>
2016-04-18 23:17:33 +0100

    wip

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
-// ----------------------------------------------------------------------------
-// Copyright (c) 2015 Microsoft Corporation. All rights reserved.
-// ABS Edit
-// ----------------------------------------------------------------------------
+var express = require('express');
+var azureMobileApps = require('azure-mobile-apps');
 
-// This is a base-level Azure Mobile App SDK.
-var express = require('express'),
-    azureMobileApps = require('azure-mobile-apps');
-
-// Set up a standard Express app
 var app = express();
 
 // If you are producing a combined Web + Mobile app, then you should handle
 // anything like logging, registering middleware, etc. here
 
-// Configuration of the Azure Mobile Apps can be done via an object, the
-// environment or an auxiliary file.  For more information, see
 // http://azure.github.io/azure-mobile-apps-node/global.html#configuration
 var mobileApp = azureMobileApps({
-    // Explicitly enable the Azure Mobile Apps home page
     homePage: true,
-    // Explicitly enable swagger support. UI support is enabled by
-    // installing the swagger-ui npm module.
-    swagger: true
+    swagger: true // UI support is enabled by installing the swagger-ui npm module.
 });
 
-// Import the files from the tables directory to configure the /tables endpoint
 mobileApp.tables.import('./tables');
-
-// Import the files from the api directory to configure the /api endpoint
 mobileApp.api.import('./api');
 
-// Initialize the database before listening for incoming requests
-// The tables.initialize() method does the initialization asynchronously
-// and returns a Promise.
-mobileApp.tables.initialize()
-    .then(function () {
-        app.use(mobileApp);    // Register the Azure Mobile Apps middleware
-        app.listen(process.env.PORT || 3000);   // Listen for requests
-    });
+mobileApp.tables.initialize().then(function () {
+    app.use(mobileApp);    // Register the Azure Mobile Apps middleware
+    app.listen(process.env.PORT || 3000);   // Listen for requests
+});
