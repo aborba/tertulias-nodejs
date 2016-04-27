ceb4a0f42b56a9b234a5144d19433962aa25dc88
Antonio Borba da Silva <abs@ggl.pt>
2016-04-27 12:04:45 +0100

    -

--- tables/Tertulias.js
+++ tables/Tertulias.js
@@ -12,7 +12,7 @@ table.dynamicSchema = false;
 table.access = 'anonymous'; // anonymous|authenticated|disabled
 
 table.read(function(context) {
-	context.query.where({ private: "false" });
+	context.query.where({ userId: context.user.id }, { private: "false" });
 	return context.execute();
 });
 
