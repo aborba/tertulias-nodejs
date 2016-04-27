b336e4aa76ced6dbbd0256a3a1d831871455f51c
Antonio Borba da Silva <abs@ggl.pt>
2016-04-27 11:43:18 +0100

    -

---
+++ tables/Tertulias.js
@@ -0,0 +1,39 @@
+var azureMobileApps = require('azure-mobile-apps');
+
+var table = azureMobileApps.table();
+
+table.columns = {
+	"title": "string",
+    "private": "boolean"
+};
+
+table.dynamicSchema = false;
+
+table.access = 'anonymous'; // anonymous|authenticated|disabled
+
+table.read(function(context) {
+	context.query.where({ private: "false" });
+	return context.execute();
+});
+
+/*
+table.read(function(context) {
+	context.query.where({ userId: context.user.id });
+	return context.execute();
+});
+
+table.insert(function(context) {
+	context.item.userId = context.user.id;
+	return context.execute();
+});
+
+table.update(function(context) {
+	return context.execute();
+});
+
+table.delete(function(context) {
+	return context.execute();
+});
+*/
+
+module.exports = table;
