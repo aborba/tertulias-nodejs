c9a1d2bbbb7eaf0ebd717d0189bb8b0f5da62338
Antonio Borba da Silva <abs@ggl.pt>
2016-04-27 16:26:21 +0100

    z

--- api/tertulias.js
+++ api/tertulias.js
@@ -4,7 +4,8 @@ var api = {
         var query = {
             sql: 'SELECT Tertulias.id AS id, Tertulias.title AS title FROM ((Tertulias INNER JOIN Members ON Tertulias.id = Members.tertulia) INNER JOIN Users ON Members.usr = Users.id) WHERE (Users.id = @userId OR Tertulias.private = @privacy)',
             parameters: [
-                { name: 'userId', value: request.query.userId },
+//                { name: 'userId', value: request.query.userId },
+                { name: 'userId', value: '4562FB8E-973A-439D-BDD6-D7FEACA29C17' },
                 { name: 'privacy', value: 0 }
             ]
         };
