154ec34f3a7eab5cb60567235d79cf68335c712d
Antonio Borba da Silva <abs@ggl.pt>
2016-04-29 16:22:22 +0100

    x

--- api/TodoItem.js
+++ api/TodoItem.js
@@ -1,24 +1,11 @@
 var api = {
+
+    all: function (req, res, next) {
+        console.log('In TodoItem all');
+    },
+
     get: function (req, res, next) {
         console.log('In TodoItem get');
-        
-    	var control = 1;
-
-/*
-        if (typeof req.params.completed === 'undefined') {
-        	console.log('-' + control++ + ': No completed parameter');
-        	res.json(result: 'none');
-        	return next();
-        }
-    	console.log('-' + control++ + ': completed parameter: ' + req.params.completed);
-        var query = {
-            sql: 'UPDATE TodoItem SET complete=@completed',
-            parameters: [{
-                completed: req.params.completed
-            }]
-        };
-    	console.log('-' + control++ + ': query: ' + query);
-*/
 
         var query = {
             sql: 'UPDATE TodoItem SET complete=@completed',
@@ -30,6 +17,7 @@ var api = {
         	res.json('Ok');
         });
     }
+    
 };
 
 //api.post.access = 'authenticated';
