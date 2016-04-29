ef5dbfaa34d3d3721fa01cc4b7db500499a48835
Antonio Borba da Silva <abs@ggl.pt>
2016-04-29 10:32:55 +0100

    z

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var app = express();
 // Configuration of the Azure Mobile Apps can be done via an object, the
 // environment or an auxiliary file.  For more information, see
 // http://azure.github.io/azure-mobile-apps-node/global.html#configuration
-var mobileApp = azureMobileApps({
+var mobile = azureMobileApps({
     // Explicitly enable the Azure Mobile Apps home page
     homePage: true,
     // Explicitly enable swagger support. UI support is enabled by
@@ -24,16 +24,16 @@ var mobileApp = azureMobileApps({
 });
 
 // Import the files from the tables directory to configure the /tables endpoint
-mobileApp.tables.import('./tables');
+mobile.tables.import('./tables');
 
 // Import the files from the api directory to configure the /api endpoint
-mobileApp.api.import('./api');
+mobile.api.import('./api');
 
 // Initialize the database before listening for incoming requests
 // The tables.initialize() method does the initialization asynchronously
 // and returns a Promise.
-mobileApp.tables.initialize()
+mobile.tables.initialize()
     .then(function () {
-        app.use(mobileApp);    // Register the Azure Mobile Apps middleware
+        app.use(mobile);    // Register the Azure Mobile Apps middleware
         app.listen(process.env.PORT || 3000);   // Listen for requests
     });
