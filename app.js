db2e75121d1f9c5f72b1901d260c1fac5db45de4
Antonio Borba da Silva <abs@ggl.pt>
2016-04-29 14:55:41 +0100

    x

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var appVersion = '1';
 
-var util = require('./util');
-util.logBanner(util.tertuliasBanner, appVersion);
+//var util = require('./util');
+//util.logBanner(util.tertuliasBanner, appVersion);
+
+var x = { tertuliasBanner: [
+        '   ______               __             ___                           ',
+        '  /\\__  _\\             /\\ \\__         /\\_ \\    __                    ',
+        '  \\/_/\\ \\/    __   _ __\\ \\ ,_\\  __  __\\//\\ \\  /\\_\\     __      ____  ',
+        '     \\ \\ \\  /\'__`\\/\\`\'__\\ \\ \\/ /\\ \\/\\ \\ \\ \\ \\ \\/\\ \\  /\'__`\\   /\',__\\ ',
+        '      \\ \\ \\/\\  __/\\ \\ \\/ \\ \\ \\_\\ \\ \\_\\ \\ \\_\\ \\_\\ \\ \\/\\ \\ \\.\\_/\\__, `\\',
+        '       \\ \\_\\ \\____\\\\ \\_\\  \\ \\__\\\\ \\____/ /\\____\\\\ \\_\\ \\__/.\\_\\/\\____/',
+        '        \\/_/\\/____/ \\/_/   \\/__/ \\/___/  \\/____/ \\/_/\\/__/\\/_/\\/___/ '
+    ]};
+for (var i = 0; i < x.tertuliasBanner.length; i++) {
+            console.log(x.tertuliasBanner[i]);
+        }
 
 var express = require('express'),
     azureMobileApps = require('azure-mobile-apps');
@@ -23,7 +36,7 @@ mobile.api.import('./api');
 console.log('Initializing...');
 mobile.tables.initialize()
     .then(function () {
-        util.dump(mobile);
+        //util.dump(mobile);
         console.log('Registering the Azure Mobile Apps middleware.');
         app.use(mobile);
         console.log('Listening for requests.');
