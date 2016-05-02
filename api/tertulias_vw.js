883b65c58edbbbd66943bbabee7a6f0929878ae8
Antonio Borba da Silva <abs@ggl.pt>
2016-05-02 20:30:16 +0100

    x

--- api/tertulias_vw.js
+++ api/tertulias_vw.js
@@ -10,6 +10,7 @@ var api = {
     get: function (req, res, next) {
         util.dumpObj(req.azureMobile.user.id);
         var sqlStr = 'SELECT * FROM Tertulias_Vw WHERE userId=@userId';
+        sqlStr = 'SELECT * FROM Tertulias_Vw WHERE private=0;';
     	var parametersArr = [{
                 userId: '\'' + req.azureMobile.user.id + '\''
             }]
