3920e00d452a5d72b8a9fab5218f19da4a03d7d2
Antonio Borba da Silva <abs@ggl.pt>
2016-05-02 13:30:42 +0100

    z

---
+++ api/membersvw.js
@@ -0,0 +1,31 @@
+var util = require('../util');
+var api = {
+
+    all: function (req, res, next) {
+        console.log('In: api/membersvw');
+        next();
+    },
+
+    get: function (req, res, next) {
+    	var sqlstr = 'SELECT * FROM Members_Vw WHERE private = 0 OR userId=@userId;';
+    	util.dumpObj(req);
+    	var parametersArr = [{
+                userId: 'context.user.userId'
+            }]
+        var query = {
+            sql: sqlstr,
+            parameters: parametersArr
+        };
+        console.log(query);
+        req.azureMobile.data.execute(query).then(function(results) {
+            res.status(200)
+            .type('application/json')
+        	.json(results);
+            return next();
+        });
+    }
+
+};
+
+//api.access = 'authenticated';
+module.exports = api;
