d5b7141695d72d5b5b8652edbbb8395fc20a8a80
Antonio Borba da Silva <abs@ggl.pt>
2016-05-03 10:37:12 +0100

    x

--- api/tertulias_vw.js
+++ api/tertulias_vw.js
@@ -15,7 +15,7 @@ var api = {
         console.log(uid);
         var uid = uid + ':';
         console.log(uid);
-        var uid = 'sid\:fadae567db0f67c6fe69d25ee8ffc0b5'; //uid + req.azureMobile.user.id.slice(4);
+        var uid = '\'sid\:fadae567db0f67c6fe69d25ee8ffc0b5\''; //uid + req.azureMobile.user.id.slice(4);
         console.log(uid);
     	var parametersArr = [{
                 _userId: uid
