bb0eef98afb0e19fe6bb9fe93191f5b44a5eaddb
Antonio Borba da Silva <abs@ggl.pt>
2016-05-05 17:26:00 +0100

    x

--- api/tertulias.js
+++ api/tertulias.js
@@ -50,7 +50,7 @@ var api = {
 
                 var request0 = new sql.Request(transaction);
                 request0.query('SELECT id FROM Users WHERE sid=\'sid:fadae567db0f67c6fe69d25ee8ffc0b5\';').then(function(rs) {
-                    console.log(rs;)
+                    console.log(rs);
                 });
 
 
