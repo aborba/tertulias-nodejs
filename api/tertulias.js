e563fa6fee0b0c53858325509c1096b04cf252ab
Antonio Borba da Silva <abs@ggl.pt>
2016-05-05 19:55:05 +0100

    c

--- api/tertulias.js
+++ api/tertulias.js
@@ -12,9 +12,8 @@ var api = {
 
     get: function (req, res, next) {
 
-        console.log('Hello!');
         var connection = new sql.Connection(util.sqlConfiguration);
-        
+
         connection.connect(function(err) {
             if (err) {
                 console.log('An error ocurred while connecting to the database. Aborting');
@@ -22,118 +21,38 @@ var api = {
                 res.sendStatus(500);
                 return;
             }
+
             var transaction = new sql.Transaction(connection);
 
             transaction.begin(function(err) {
 
-                if (err) {
-                    transaction.rollback();
-                    return;
-                }
-                var rolledback = false;
+                if (err) { transaction.rollback(); return; }
 
-                transaction.on('rollback', function(aborted) {
-                    rolledback = true;
-                });
+                var rolledback = false;
+                transaction.on('rollback', function(aborted) { rolledback = true; });
 
                 var sqlRequest = new sql.Request(transaction);
 
                 var _userId = "";
 
-                console.log('req.azureMobile.user.id: ' + req.azureMobile.user.id);
+                var queryString = 'SELECT id FROM Users WHERE sid=@sid;';
 
                 var preparedStatement = new sql.PreparedStatement();
 
                 preparedStatement.input('sid', sql.String);
 
-                preparedStatement.prepare('SELECT id FROM Users WHERE sid=@sid;', funtion(err) {
-                    preparedStatement.execute({sid: req.azureMobile.user.id}, function(err, recordset, affected) {
-                        console.log('preparedStatement result');
-                        console.log(recordset);
-                    });
-                    preparedStatement.unprepare(function(err) {
-                    });
-                });
-
-                sqlRequest.query('SELECT id FROM Users WHERE sid=\'sid:fadae567db0f67c6fe69d25ee8ffc0b5\';')
-                .then(function(rs) {
-                    _userId = rs[0].id;
-                    return _userId;
-                }).then(function(par) {
-                    console.log("par: " + par);
-                    console.log("Read User Id (1): " + _userId);
-                });
-                
-                console.log("Read User Id (0): " + _userId);
-
-
-                // TO HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-                var request = new sql.Request(transaction);
-                console.log('request built');
-                request.query('SELECT DISTINCT tertuliaId AS id, ' +
-                          'tertuliaTitle AS title, ' +
-                          'tertuliaSubject AS subject, ' +
-                          'tertuliaSchedule AS schedule, ' +
-                          'tertuliaPrivate AS private ' +
-                      'FROM Tertulias_Vw ' +
-                      'WHERE tertuliaPrivate=0',
-                    function(err, recordset) {
-                        console.log('select result');
-                        if (err) {
-                            if (!rolledBack) {
-                                console.log('trying to rollback');
-                                transaction.rollback(function(err) {
-                                    console.log('rolling back');
-                                });
-                            }
-                        } else {
-                            transaction.commit(function(err) {
-                                if (err) {
-                                    if (!rolledBack) {
-                                        console.log('trying to rollback');
-                                        transaction.rollback(function(err) {
-                                            console.log('rolling back');
-                                        });
-                                    }
-                                }
-                                console.log('committed');
-                            });
-                            console.log('recordset');
+                preparedStatement.prepare(queryString, funtion(err) {
+                    preparedStatement.execute({ sid: req.azureMobile.user.id }, 
+                        function(err, recordset, affected) {
+                            console.log('preparedStatement result');
                             console.log(recordset);
-                        }
-                    });
-                if (err) {
-                    console.log('trying to rollback');
-                    transaction.rollback();
-                }
-            });
-        });
+                        });
+                    preparedStatement.unprepare(function(err) { });
+                });
 
-/*
-        var query = {
-            sql: 'SELECT DISTINCT tertuliaId AS id, ' +
-                      'tertuliaTitle AS title, ' +
-                      'tertuliaSubject AS subject, ' +
-                      'tertuliaSchedule AS schedule, ' +
-                      'tertuliaPrivate AS private ' +
-                  'FROM Tertulias_Vw ' +
-                  'WHERE tertuliaPrivate=@privacy OR userId=@userId',
-            parameters: [
-                { 'name': 'privacy', 'value': '0'},
-                { 'name': 'userId', 'value': req.azureMobile.user.id }
-            ]
-        };
-        console.log(query);
-        req.azureMobile.data.execute(query).then(function(results) {
-            console.log('results');
-            console.log(results);
-            res.status(200)
-                .type('application/json')
-                .json(results);
-            return next();
-        });
-*/
+            })
+
+        })
     },
 
     post: function (req, res, next) {
