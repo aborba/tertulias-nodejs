be46f71e87bab871e7bd4c150a46b4b0ca161797
Antonio Borba da Silva <abs@ggl.pt>
2016-05-05 17:29:49 +0100

    x

--- api/tertulias.js
+++ api/tertulias.js
@@ -52,7 +52,7 @@ var api = {
                 var _userId;
                 request0.query('SELECT id FROM Users WHERE sid=\'sid:fadae567db0f67c6fe69d25ee8ffc0b5\';').then(function(rs) {
                     console.log(rs);
-                    _userId = rs.id;
+                    _userId = rs[0].id;
                 });
                 console.log("Read User Id: " + _userId);
 
