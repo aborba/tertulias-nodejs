4070cf1fce674e98e47d8f784c872e8643bbba13
Antonio Borba da Silva <abs@ggl.pt>
2016-05-06 18:23:03 +0100

    another

--- api/register.js
+++ api/register.js
@@ -4,89 +4,42 @@ var sql = require('mssql');
 
 var api = {
 
-    all: function (req, res, next) {
-        console.log('In: api/register');
-        return next();
-    },
-
-    get: function (req, res, next) {
-    	console.log('in GET');
-    },
-
     post: function (req, res, next) {
-    	console.log('in POST');
         var connection = new sql.Connection(util.sqlConfiguration);
         connection.connect(function(err) {
-            if (err) {console.log('control point 0'); console.log(isError); res.sendStatus(500); return; }
-            console.log('control point 1'); 
+            if (err) { console.log(isError); res.sendStatus(500); return; }
             var transaction = new sql.Transaction(connection);
-            console.log('control point 2'); 
             transaction.begin(function(err) {
-            console.log('control point 3');
-                if (err) { console.log('control point 4'); transaction.rollback(); return; }
-	            console.log('control point 5');
+                if (err) { transaction.rollback(); return; }
                 var rolledback = false;
-	            console.log('control point 6');
-                transaction.on('rollback', function(aborted) { console.log('control point 7'); rolledback = true; res.sendStatus(500); });
-            	console.log('control point 8');
+                transaction.on('rollback', function(aborted) { rolledback = true; res.sendStatus(500); });
                 var sqlRequest = new sql.Request(transaction);
-            	console.log('control point 9');
-                var _userId = '';
-            	console.log('control point 10');
                 var queryString = 'SELECT id FROM Users WHERE sid=@sid;';
-            	console.log('control point 11');
-                var preparedStatement_0 = new sql.PreparedStatement(connection);
-            	console.log('control point 12');
-                //transaction.on('commit', function(succeeded) { preparedStatement_0.unprepare(); res.sendStatus(200); });
-                //transaction.on('rollback', function(aborted) { rolledback = true; preparedStatement_0.unprepare(); res.sendStatus(500); });
-            	console.log('control point 13');
-                preparedStatement_0.input('sid', sql.NVarChar);
-            	console.log('control point 14');
-                preparedStatement_0.prepare(queryString, function(err) {
-            		console.log('control point 15');
-                    if (err) { console.log('control point 16'); transaction.rollback(); return; }
-            		console.log('control point 17');
-                    preparedStatement_0.execute({ sid: req.azureMobile.user.id }, 
+                var preparedStatement = new sql.PreparedStatement(connection);
+                transaction.on('commit', function(succeeded) { preparedStatement.unprepare(); res.sendStatus(200); });
+                transaction.on('rollback', function(aborted) { rolledback = true; preparedStatement.unprepare(); res.sendStatus(500); });
+                preparedStatement.input('sid', sql.NVarChar);
+                preparedStatement.prepare(queryString, function(err) {
+                    if (err) { transaction.rollback(); return; }
+                    preparedStatement.execute({ sid: req.azureMobile.user.id }, 
                         function(err, recordset, affected) {
-                        	console.log('control point 18'); 
-                            if (err) {console.log('control point 19'); transaction.rollback(); return; }
-                            if (typeof recordset != 'undefined' && recordset[0] != null) {console.log('control point 20'); transaction.commit(); return; }
-							console.log('control point 21');
-                            var preparedStatement_1 = new sql.PreparedStatement(connection);
-							console.log('control point 22');
-			                transaction.on('commit', function(succeeded) {
-			                	preparedStatement_0.unprepare();
-			                	preparedStatement_1.unprepare();
-			                	res.sendStatus(200);
-			                });
-			                transaction.on('rollback', function(aborted) {
-			                	rolledback = true;
-			                	preparedStatement_0.unprepare();
-			                	preparedStatement_1.unprepare();
-			                	res.sendStatus(500);
-			                });
+                            if (err) { transaction.rollback(); return; }
+                            if (typeof recordset != 'undefined' && recordset[0] != null) { transaction.commit(); return; }
+                            preparedStatement.unprepare();
                             queryString = 'INSERT INTO Users (sid) values (@sid);';
-							console.log('control point 23');
-			                preparedStatement_1.input('sid', sql.NVarChar);
-							console.log('control point 24');
-			                preparedStatement_1.prepare(queryString, function(err) {
-			                	console.log('control point 25');
-			                    if (err) { console.log('control point 26'); transaction.rollback(); return; }
-								console.log('control point 27');
-			                    preparedStatement_1.execute({ sid: req.azureMobile.user.id }, 
+			                preparedStatement.input('sid', sql.NVarChar);
+			                preparedStatement.prepare(queryString, function(err) {
+			                    if (err) { transaction.rollback(); return; }
+			                    preparedStatement.execute({ sid: req.azureMobile.user.id }, 
 			                        function(err, recordset, affected) {
-			                        	console.log('control point 28'); 
-					                    if (err) { console.log('control point 29'); console.log(err); transaction.rollback(); return; }
-			                        	console.log('control point 30'); 
+					                    if (err) { consoleconsole.log(err); transaction.rollback(); return; }
 					                    transaction.commit();
-			                        	console.log('control point 31'); 
 			                        }
 		                        );
 			                });
                         }
                     );
                  });
-                console.log('control point 31'); 
             });
         });
 
