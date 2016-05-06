4cc9f3bfb03e7ef004c998e1702442cd420b8ee8
Antonio Borba da Silva <abs@ggl.pt>
2016-05-06 10:14:53 +0100

    another

--- api/tertulias.js
+++ api/tertulias.js
@@ -60,8 +60,6 @@ var api = {
         console.log('In: post');
         console.log(req.azureMobile.user.id);
         console.log(req.body);
-        console.log('req.azureMobile.connection');
-        console.log(req.azureMobile.connection);
         var query = {
             sql: 'INSERT INTO Terulias(title, subject, schedule, privacy) VALUES (@title, @subject, @schedule, @privacy)',
             parameters: [
