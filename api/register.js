a36126d7384d0ea372da497b4722fb0ad4ef3202
Antonio Borba da Silva <abs@ggl.pt>
2016-05-07 22:10:30 +0100

    another

--- api/register.js
+++ api/register.js
@@ -6,6 +6,67 @@ var queryInsertSid = 'INSERT INTO Users (sid) values (@sid);';
 
 var tranDone = false;
 
+var api = {
+	post: function (req, res, next) {
+		var conn = new sql.Connection(util.sqlConfiguration);
+		conn.connect(function(err) {
+			if (err) { completeError(err, res); return; }
+			var tran = new sql.Transaction(conn);
+			tran.begin(function(err) {
+				if (err) { rollback500(err, res, tran); return; }
+				var sqlRequest = new sql.Request(tran);
+				var psSelectId = new sql.PreparedStatement(conn);
+				psSelectId.input('sid', sql.NVarChar);
+				psSelectId.prepare(querySelectId, function(err) {
+					if (err) { rollback500(err, res, tran); return; }
+					psSelectId.execute({ sid: req.azureMobile.user.id }, 
+						function(err, recordset, affected) {
+							if (err) { rollback500(err, res, tran); return; }
+							if (typeof recordset != 'undefined' && recordset[0] != null) { rollback200(res, tran); return; }
+							psSelectId.unprepare();
+							/*
+							var psInsertSid = new sql.PreparedStatement(conn);
+							psInsertSid.input('sid', sql.NVarChar);
+							psInsertSid.prepare(queryInsertSid, function(err) {
+								if (err) { rollback500(err, res, tran); return; }
+								psInsertSid.execute({ sid: req.azureMobile.user.id }, 
+									function(err, recordset, affected) {
+										if (err) { rollback500(err, res, tran); return; }
+										commit200(res, tran);
+									}
+								);
+							});
+							*/
+							userName(req.azureMobile.user, function() {
+								var psInsertSid = new sql.PreparedStatement(conn);
+								psInsertSid.input('sid', sql.NVarChar);
+								psInsertSid.prepare(queryInsertSid, function(err) {
+									if (err) { rollback500(err, res, tran); return; }
+									psInsertSid.execute({ sid: req.azureMobile.user.id }, 
+										function(err, recordset, affected) {
+											if (err) { rollback500(err, res, tran); return; }
+											commit200(res, tran);
+										}
+									);
+								});
+							});
+						}
+					);
+				});
+			});
+		});
+    }
+};
+
+api.access = 'authenticated';
+
+var userName = function(user, next) {
+    user.getIdentity().then(function(_identity){
+    	console.log(_identity);
+    	next();
+    });
+};
+
 var completeError = function(err, res) {
 	if (err) {
 		console.log(err);
@@ -51,69 +112,5 @@ var commit200 = function(res, tran) {
 		sendStatus: 200
 	});
 }
-
-var api = {
-	post: function (req, res, next) {
-		var conn = new sql.Connection(util.sqlConfiguration);
-		conn.connect(function(err) {
-			if (err) { completeError(err, res); return; }
-			var tran = new sql.Transaction(conn);
-			tran.begin(function(err) {
-				if (err) { rollback500(err, res, tran); return; }
-				var sqlRequest = new sql.Request(tran);
-				var psSelectId = new sql.PreparedStatement(conn);
-				psSelectId.input('sid', sql.NVarChar);
-				psSelectId.prepare(querySelectId, function(err) {
-					if (err) { rollback500(err, res, tran); return; }
-					psSelectId.execute({ sid: req.azureMobile.user.id }, 
-						function(err, recordset, affected) {
-							if (err) { rollback500(err, res, tran); return; }
-							if (typeof recordset != 'undefined' && recordset[0] != null) { rollback200(res, tran); return; }
-							psSelectId.unprepare();
-							var psInsertSid = new sql.PreparedStatement(conn);
-							psInsertSid.input('sid', sql.NVarChar);
-							psInsertSid.prepare(queryInsertSid, function(err) {
-								if (err) { rollback500(err, res, tran); return; }
-								psInsertSid.execute({ sid: req.azureMobile.user.id }, 
-									function(err, recordset, affected) {
-										if (err) { rollback500(err, res, tran); return; }
-										commit200(res, tran);
-									}
-								);
-							});
-								/*
-							userName(req.azureMobile.user, function() {
-								var psInsertSid = new sql.PreparedStatement(conn);
-								psInsertSid.input('sid', sql.NVarChar);
-								psInsertSid.prepare(queryInsertSid, function(err) {
-									if (err) { rollback500(err, res, tran); return; }
-									psInsertSid.execute({ sid: req.azureMobile.user.id }, 
-										function(err, recordset, affected) {
-											if (err) { rollback500(err, res, tran); return; }
-											commit200(res, tran);
-										}
-									);
-								});
-							});
-								*/
-						}
-					);
-				});
-			});
-		});
-    }
-};
-
-api.access = 'authenticated';
-
-/*
-var userName = function(user, next) {
-    user.getIdentity().then(function(_identity){
-    	console.log(_identity);
-    	next();
-    });
-};
-*/
-
 
 module.exports = api;
