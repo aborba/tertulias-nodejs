602c11c6cfa8726193adaff793a0e84df6dda96a
Antonio Borba da Silva <abs@ggl.pt>
2016-05-07 21:37:27 +0100

    another

--- api/register.js
+++ api/register.js
@@ -6,58 +6,6 @@ var queryInsertSid = 'INSERT INTO Users (sid) values (@sid);';
 
 var tranDone = false;
 
-var api = {
-	post: function (req, res, next) {
-		var conn = new sql.Connection(util.sqlConfiguration);
-		conn.connect(function(err) {
-			if (err) { completeError(err, res); return; }
-			var tran = new sql.Transaction(conn);
-			tran.begin(function(err) {
-				if (err) { rollback500(err, res, tran); return; }
-				var sqlRequest = new sql.Request(tran);
-				var psSelectId = new sql.PreparedStatement(conn);
-				psSelectId.input('sid', sql.NVarChar);
-				psSelectId.prepare(querySelectId, function(err) {
-					if (err) { rollback500(err, res, tran); return; }
-					psSelectId.execute({ sid: req.azureMobile.user.id }, 
-						function(err, recordset, affected) {
-							if (err) { rollback500(err, res, tran); return; }
-							if (typeof recordset != 'undefined' && recordset[0] != null) { rollback200(res, tran); return; }
-							psSelectId.unprepare();
-								/*
-							userName(req.azureMobile.user, function() {
-								var psInsertSid = new sql.PreparedStatement(conn);
-								psInsertSid.input('sid', sql.NVarChar);
-								psInsertSid.prepare(queryInsertSid, function(err) {
-									if (err) { rollback500(err, res, tran); return; }
-									psInsertSid.execute({ sid: req.azureMobile.user.id }, 
-										function(err, recordset, affected) {
-											if (err) { rollback500(err, res, tran); return; }
-											commit200(res, tran);
-										}
-									);
-								});
-							});
-								*/
-						}
-					);
-				});
-			});
-		});
-    }
-};
-
-api.access = 'authenticated';
-
-/*
-var userName = function(user, next) {
-    user.getIdentity().then(function(_identity){
-    	console.log(_identity);
-    	next();
-    });
-};
-*/
-
 var completeError = function(err, res) {
 	if (err) {
 		console.log(err);
@@ -103,5 +51,59 @@ var commit200 = function(res, tran) {
 		sendStatus: 200
 	});
 }
+
+var api = {
+	post: function (req, res, next) {
+		var conn = new sql.Connection(util.sqlConfiguration);
+		conn.connect(function(err) {
+			if (err) { completeError(err, res); return; }
+			var tran = new sql.Transaction(conn);
+			tran.begin(function(err) {
+				if (err) { rollback500(err, res, tran); return; }
+				var sqlRequest = new sql.Request(tran);
+				var psSelectId = new sql.PreparedStatement(conn);
+				psSelectId.input('sid', sql.NVarChar);
+				psSelectId.prepare(querySelectId, function(err) {
+					if (err) { rollback500(err, res, tran); return; }
+					psSelectId.execute({ sid: req.azureMobile.user.id }, 
+						function(err, recordset, affected) {
+							if (err) { rollback500(err, res, tran); return; }
+							if (typeof recordset != 'undefined' && recordset[0] != null) { rollback200(res, tran); return; }
+							psSelectId.unprepare();
+								/*
+							userName(req.azureMobile.user, function() {
+								var psInsertSid = new sql.PreparedStatement(conn);
+								psInsertSid.input('sid', sql.NVarChar);
+								psInsertSid.prepare(queryInsertSid, function(err) {
+									if (err) { rollback500(err, res, tran); return; }
+									psInsertSid.execute({ sid: req.azureMobile.user.id }, 
+										function(err, recordset, affected) {
+											if (err) { rollback500(err, res, tran); return; }
+											commit200(res, tran);
+										}
+									);
+								});
+							});
+								*/
+							rollback200(res, tran);
+						}
+					);
+				});
+			});
+		});
+    }
+};
+
+api.access = 'authenticated';
+
+/*
+var userName = function(user, next) {
+    user.getIdentity().then(function(_identity){
+    	console.log(_identity);
+    	next();
+    });
+};
+*/
+
 
 module.exports = api;
