443eb2e3cc9cabbff05fac0b0037265ba7b5b247
Antonio Borba da Silva <abs@ggl.pt>
2016-05-07 18:33:55 +0100

    another

--- api/profile.js
+++ api/profile.js
@@ -3,6 +3,7 @@ var sql = require('mssql');
 
 var querySelectUser = 'SELECT * FROM Users WHERE sid=@sid;';
 var querySelectId = 'SELECT id FROM Users WHERE sid=@sid;';
+var queryUpdateUser = 'UPDATE Users (alias) values (@alias) WHERE sid=@sid;';
 
 var tranDone = false;
 
@@ -39,35 +40,56 @@ var api = {
 		console.log('alias: ' + req.body.alias);
 		var connection = new sql.Connection(util.sqlConfiguration);
 		connection.connect(function(err) {
+			console.log('control point 1');
 			if (err) { rollback(err, res, transaction); return; }
+			console.log('control point 2');
 			var transaction = new sql.Transaction(connection);
+			console.log('control point 3');
 			transaction.begin(function(err) {
+			console.log('control point 4');
 				if (err) { rollback(err, res, transaction); return; }
+			console.log('control point 5');
 				var sqlRequest = new sql.Request(transaction);
-				var queryString = 'SELECT id FROM Users WHERE sid=@sid;';
+			console.log('control point 6');
 				var preparedStatement = new sql.PreparedStatement(connection);
+			console.log('control point 7');
 				transaction.on('commit', function(succeeded) { preparedStatement.unprepare(); res.sendStatus(200); });
 				transaction.on('rollback', function(aborted) { rolledback = true; preparedStatement.unprepare(); res.sendStatus(500); });
+			console.log('control point 8');
 				preparedStatement.input('sid', sql.NVarChar);
-				preparedStatement.prepare(queryString, function(err) {
+			console.log('control point 9');
+				preparedStatement.prepare(querySelectId, function(err) {
+			console.log('control point 10');
 					if (err) { rollback(err, res, transaction); return; }
+			console.log('control point 11');
 					preparedStatement.execute({ sid: req.azureMobile.user.id }, 
 						function(err, recordset, affected) {
+			console.log('control point 12');
 							if (err) { rollback(err, res, transaction); return; }
+			console.log('control point 13');
 							if (typeof recordset != 'undefined' && recordset[0] != null) { transaction.commit(); return; }
+			console.log('control point 14');
 							preparedStatement.unprepare();
-							queryString = 'UPDATE Users (alias) values (@alias) WHERE sid=@sid;';
+			console.log('control point 15');
 							preparedStatement.input('sid', sql.NVarChar);
+			console.log('control point 16');
 							preparedStatement.input('alias', sql.NVarChar);
-							preparedStatement.prepare(queryString, function(err) {
+			console.log('control point 17');
+							preparedStatement.prepare(queryUpdateUser, function(err) {
+			console.log('control point 18');
 								if (err) { rollback(err, res, transaction); return; }
+			console.log('control point 19');
 								console.log('alias: ' + req.body.alias);
+			console.log('control point 20');
 								preparedStatement.execute({
 									sid: req.azureMobile.user.id,
 									alias: req.body.alias || ""
 								}, function(err, recordset, affected) {
+			console.log('control point 21');
 									if (err) { rollback(err, res, transaction); return; }
+			console.log('control point 22');
 									commit(res, transaction);
+			console.log('control point 23');
 								});
 							});
 						}
