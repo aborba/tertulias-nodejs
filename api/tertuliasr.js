b2b874314f229620cf66172352e111d80430cdf4
Antonio Borba da Silva <abs@ggl.pt>
2016-05-08 22:59:22 +0100

    another

--- api/tertuliasr.js
+++ api/tertuliasr.js
@@ -3,33 +3,39 @@ var express = require('express');
 var util = require('../util');
 var sql = require('mssql');
 
-var router = express.Router();
+var authenticate = require('azure-mobile-apps/src/express/middleware/authenticate'),
+    authorize = require('azure-mobile-apps/src/express/middleware/authorize');
 
-router.get('/', function (req, res) {
-    var connection = new sql.Connection(util.sqlConfiguration);
-    connection.connect(function(err) {
-        var sqlRequest = new sql.Request(connection);
-        var preparedStatement = new sql.PreparedStatement(connection);
-        /* String -> sql.NVarChar; Number -> sql.Int; Boolean -> sql.Bit; Date -> sql.DateTime;
-           Buffer -> sql.VarBinary; sql.Table -> sql.TVP */
-        preparedStatement.input('sid', sql.NVarChar);
-        preparedStatement.prepare(querySelectTertulias, function(err) {
-            if (err) { completeError(err, res); return; }
-            preparedStatement.execute({ sid: req.azureMobile.user.id }, 
-                function(err, recordset, affected) {
-                    if (err) { completeError(err, res); return; }
-                    console.log(recordset);
-                    preparedStatement.unprepare();
-                    res.type('application/json').json(recordset);
-                }
-            );
-         });
+module.exports = function (configuration) {
+    var router = express.Router();
+
+    router.get('/', authenticate(configuration), authorize, function (req, res) {
+        var connection = new sql.Connection(util.sqlConfiguration);
+        connection.connect(function(err) {
+            var sqlRequest = new sql.Request(connection);
+            var preparedStatement = new sql.PreparedStatement(connection);
+            /* String -> sql.NVarChar; Number -> sql.Int; Boolean -> sql.Bit; Date -> sql.DateTime;
+               Buffer -> sql.VarBinary; sql.Table -> sql.TVP */
+            preparedStatement.input('sid', sql.NVarChar);
+            preparedStatement.prepare(querySelectTertulias, function(err) {
+                if (err) { completeError(err, res); return; }
+                preparedStatement.execute({ sid: req.azureMobile.user.id }, 
+                    function(err, recordset, affected) {
+                        if (err) { completeError(err, res); return; }
+                        console.log(recordset);
+                        preparedStatement.unprepare();
+                        res.type('application/json').json(recordset);
+                    }
+                );
+             });
+        });
     });
-});
 
-router.get('/page/:nr', function (req, res) {
-	console.log('par ' + req.params.nr);
-});
+    router.get('/page/:nr', authenticate(configuration), authorize, function (req, res) {
+    	console.log('par ' + req.params.nr);
+    });
 
-router.access = 'authenticated';
-module.exports = router;
+    return router;
+}
+
+//router.access = 'authenticated';
