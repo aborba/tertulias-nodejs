5c833a07cdccb5bfb3025015cb177b55bd044b34
Antonio Borba da Silva <abs@ggl.pt>
2016-06-07 17:47:46 +0100

    x

--- api/mytertulias.js
+++ api/mytertulias.js
@@ -19,19 +19,22 @@ var queryLocations = 'SELECT DISTINCT lo_id, lo_name, lo_address, lo_zip, lo_cou
 ' WHERE tr_is_cancelled = 0 AND us_sid = @sid' +
 ' AND tr_id = @tertulia';
 
-var queryDefaultLocation = 'SELECT lo_id, lo_name, lo_address, lo_zip, lo_country, lo_latitude, lo_longitude' +
-' FROM Locations' +
-' INNER JOIN Tertulias ON lo_tertulia = tr_id' +
-' INNER JOIN Members ON mb_tertulia = tr_id' +
-' INNER JOIN Users ON mb_user = us_id' +
-' WHERE tr_is_cancelled = 0 AND us_sid = @sid' +
-' AND tr_id = @tertulia'
+var queryDefaultLocation = queryLocations +
 ' AND lo_id = (SELECT tr_location FROM Tertulias WHERE tr_id = @tertulia)';
 
-var queryScheduleType =  'SELECT nv_name' +
+var queryScheduleType = 'SELECT nv_name' +
 ' FROM Tertulias' +
 ' INNER JOIN Schedules ON tr_schedule = sc_id' +
 ' INNER JOIN EnumValues ON sc_recurrency = nv_id' +
+' INNER JOIN Members ON mb_tertulia = tr_id' +
+' INNER JOIN Users ON mb_user = us_id' +
+' WHERE tr_is_cancelled = 0 AND us_sid = @sid' +
+' AND tr_id = @tertulia';
+
+var queryScheduleMonthlyW = 'SELECT *' +
+' FROM MonthlyW' +
+' INNER JOIN Schedules ON mw_schedule = sc_id' +
+' INNER JOIN Tertulias ON tr_schedule = sc_id' +
 ' INNER JOIN Members ON mb_tertulia = tr_id' +
 ' INNER JOIN Users ON mb_user = us_id' +
 ' WHERE tr_is_cancelled = 0 AND us_sid = @sid' +
@@ -56,59 +59,48 @@ var api = {
         var paramsT = [];
         var paramsV = {};
         var tr_id = req.query.id;
-        console.log('id: ' + tr_id);
         if (typeof tr_id === typeof undefined) {
             console.log('Preparing to get all my Tertulias');
             selectedQuery = queryTertulias;
             paramsT['sid'] = sql.NVarChar; // String -> sql.NVarChar; Number -> sql.Int; Boolean -> sql.Bit; Date -> sql.DateTime; Buffer -> sql.VarBinary; sql.Table -> sql.TVP
-            paramsV = {
-                'sid': req.azureMobile.user.id
-            };
+            paramsV = {'sid': req.azureMobile.user.id };
         } else {
             var sub = req.query.sub;
+            paramsT['sid'] = sql.NVarChar; paramsT['tertulia'] = sql.NVarChar;
+            paramsV = {'sid': req.azureMobile.user.id, 'tertulia': tr_id };
             if (typeof sub === typeof undefined) {
-                console.log('Preparing to get my Tertulia with id: ' + tr_id);
                 selectedQuery = queryTertuliaX;
-                paramsT['sid'] = sql.NVarChar;
-                paramsT['tertulia'] = sql.NVarChar;
-                paramsV = {
-                    'sid': req.azureMobile.user.id,
-                    'tertulia': tr_id
-                };
             } else {
                 switch (sub) {
                     case 'locations':
-                        console.log('Preparing to get all the ' + sub + ' of my Tertulia with id: ' + tr_id);
                         selectedQuery = queryLocations;
-                        paramsT['sid'] = sql.NVarChar;
-                        paramsT['tertulia'] = sql.NVarChar;
-                        paramsV = {
-                            'sid': req.azureMobile.user.id,
-                            'tertulia': tr_id
-                        };
                         break;
                     case 'defaultLocation':
                         console.log('Preparing to get the ' + sub + ' of my Tertulia with id: ' + tr_id);
                         selectedQuery = queryDefaultLocation;
-                        paramsT['sid'] = sql.NVarChar;
-                        paramsT['tertulia'] = sql.NVarChar;
-                        paramsV = {
-                            'sid': req.azureMobile.user.id,
-                            'tertulia': tr_id
-                        };
                         break;
                     case 'scheduleType':
                         console.log('Preparing to get the ' + sub + ' of my Tertulia with id: ' + tr_id);
                         selectedQuery = queryScheduleType;
-                        paramsT['sid'] = sql.NVarChar;
-                        paramsT['tertulia'] = sql.NVarChar;
-                        paramsV = {
-                            'sid': req.azureMobile.user.id,
-                            'tertulia': tr_id
-                        };
                         break;
+                    case 'schedule':
+                        console.log('Preparing to get the ' + sub + ' of my Tertulia with id: ' + tr_id);
+                        var schedule_type = req.query.type;
+                        if (typeof schedule_type === typeof undefined) {
+                            res.sendStatus(400);
+                            return;
+                        }
+                        console.log('Preparing to get the ' + schedule_type + ' ' + sub + ' of my Tertulia with id: ' + tr_id);
+                        switch (schedule_type) {
+                            case 'MonthlyW':
+                                selectedQuery = queryScheduleMonthlyW;
+                                break;
+                            default:
+                                res.sendStatus(400);
+                                return;
+                        }
                     default:
-                        res.sendStatus(500);
+                        res.sendStatus(400);
                         return;
                 }
             }
@@ -133,38 +125,6 @@ var api = {
         });
     }
 };
-
-/*
-var api = {
-
-    all: function (req, res, next) {
-        console.log('In: api/tertulias');
-        return next();
-    },
-
-    get: function (req, res, next) {
-        var connection = new sql.Connection(util.sqlConfiguration);
-        connection.connect(function(err) {
-            var sqlRequest = new sql.Request(connection);
-            var preparedStatement = new sql.PreparedStatement(connection);
-            // String -> sql.NVarChar; Number -> sql.Int; Boolean -> sql.Bit; Date -> sql.DateTime;
-            // Buffer -> sql.VarBinary; sql.Table -> sql.TVP
-            preparedStatement.input('sid', sql.NVarChar);
-            preparedStatement.prepare(queryTertulias, function(err) {
-                if (err) { completeError(err, res); return; }
-                preparedStatement.execute({ sid: req.azureMobile.user.id }, 
-                    function(err, recordset, affected) {
-                        if (err) { completeError(err, res); return; }
-                        console.log(recordset);
-                        preparedStatement.unprepare();
-                        res.type('application/json').json(recordset);
-                    }
-                );
-             });
-        });
-    }
-};
-*/
 
 api.access = 'authenticated';
 module.exports = api;
