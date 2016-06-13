47b8c42aa64379eb842d10014fa62f7067391963
Antonio Borba da Silva <abs@ggl.pt>
2016-06-13 13:07:13 +0100

    x

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ util.logTertulias2(appVersion);
 util.nodeVersion();
 
 var express = require('express'),
-    azureMobileApps = require('azure-mobile-apps');
-//me
-var bodyParser = require('body-parser');
-var router = express.Router();
+    azureMobileApps = require('azure-mobile-apps'),
+    tertulias_routes = require('./tertulias_routes');
 
 var app = express();
 
@@ -19,96 +17,9 @@ var appConfiguration = {   // http://azure.github.io/azure-mobile-apps-node/glob
     skipVersionCheck: true
 }
 
-//me
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 var mobile = azureMobileApps(appConfiguration);
 
 mobile.api.import('./api');
-
-/*
-function calculateAndRespond(x, y, op, res) {
-    var result = 3;
-    switch (op) {
-        case 'add':
-            result = parseInt(x, 10) + parseInt(y, 10);
-            break;
-        case 'sub':
-            result = parseInt(x, 10) - parseInt(y, 10);
-            break;
-        default:
-            res.send(400, 'Error');
-    }
-    if (op === 'add') {
-        result = parseInt(x, 10) + parseInt(y, 10);
-    } else if (op === 'sub') {
-        result = parseInt(x, 10) - parseInt(y, 10);
-    } else {
-        res.send(400, { error: 'Operation "' + op + '" not supported' });
-    }
-    res.send(200, { result: result });
-}
-
-function getImplementation(req, res) {
-    console.log('In calculator');
-    console.log('operation = ' + operation);
-    console.log('x = ' + x);
-    console.log('y = ' + y);
-    var x = parseInt(req.query.x, 10);
-    var y = parseInt(req.query.y, 10);
-    var path = req.path;
-    var operation = path.substring('/api/calculator/'.length);
-    calculateAndRespond(x, y, operation, res);
-}
-
-mobile.api.register = function (api) {
-    console.log('Registering');
-    api.get('*', getImplementation);
-}
-*/
-
-function calculateAndRespond(x, y, op, res) {
-    var result = 3;
-    switch (op) {
-        case 'add':
-            result = parseInt(x, 10) + parseInt(y, 10);
-            break;
-        case 'sub':
-            result = parseInt(x, 10) - parseInt(y, 10);
-            break;
-        default:
-            res.send(400, 'Error');
-    }
-    if (op === 'add') {
-        result = parseInt(x, 10) + parseInt(y, 10);
-    } else if (op === 'sub') {
-        result = parseInt(x, 10) - parseInt(y, 10);
-    } else {
-        res.send(400, { error: 'Operation "' + op + '" not supported' });
-    }
-    res.send(200, { result: result });
-}
-
-function getImplementation(req, res) {
-    console.log('In calculator');
-    console.log('operation = ' + operation);
-    console.log('x = ' + x);
-    console.log('y = ' + y);
-    var x = parseInt(req.query.x, 10);
-    var y = parseInt(req.query.y, 10);
-    var path = req.path;
-    var operation = path.substring('/calculator/'.length);
-    console.log(operation);
-    calculateAndRespond(x, y, operation, res);
-}
-
-function getImplementation2(req, res) {
-    console.log('In Implementation2');
-    res.status(200).send({ result: 'OK' });
-}
-
-var tertulias_routes = require('./tertulias_routes');
 
 console.log('Initializing...');
 mobile.tables
@@ -117,7 +28,7 @@ mobile.tables
     //util.dumpObj(mobile);
     console.log('Registering the Azure Mobile Apps middleware.');
     app.use(mobile);
-    app.use('/t', tertulias_routes);
+    app.use('/tertulias', tertulias_routes);
     console.log('Listening for requests.');
     app.listen(process.env.PORT || 3000);
 });
--- tertulias_routes.js
+++ tertulias_routes.js
@@ -1,7 +1,33 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    azureMobileApps = require('azure-mobile-apps');
+var util = require('../util');
+var u = require('azure-mobile-apps/src/auth/user');
+var sql = require('mssql');
 
-var statusOK = 200;
+var router = express.Router();
+var miniapp = express();
+
+const statusOK = 200;
+
+var appConfiguration = {   // http://azure.github.io/azure-mobile-apps-node/global.html#configuration
+    debug: true,
+    homePage: true,
+    swagger: false,
+    skipVersionCheck: true
+}
+
+var mobile = azureMobileApps(appConfiguration);
+
+const queryTertulias = 'SELECT DISTINCT tr_id, tr_name, tr_subject, lo_address, lo_zip, lo_country, lo_latitude, lo_longitude, sc_recurrency, tr_is_private, nv_name' +
+' FROM Tertulias' +
+' INNER JOIN Locations  ON tr_location = lo_id' +
+' INNER JOIN Schedules  ON tr_schedule = sc_id' +
+' INNER JOIN Members    ON mb_tertulia = tr_id' +
+' INNER JOIN Users      ON mb_user = us_id' +
+' INNER JOIN EnumValues ON mb_role = nv_id' +
+' WHERE tr_is_cancelled = 0 AND us_sid = @sid';
+
+const queryTertuliaX = queryTertulias + ' AND tr_id = @tertulia';
 
 router.use(function timeLog(req, res, next) {
 	console.log('Time: ', Date.now());
@@ -9,50 +35,70 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get('/', function(req, res) {
-	res.status(statusOK)
-		.send('Tertulias list');
+    var selectedQuery = queryTertulias;
+    var paramsT = [];
+    paramsT['sid'] = sql.NVarChar; // String -> sql.NVarChar; Number -> sql.Int; Boolean -> sql.Bit; Date -> sql.DateTime; Buffer -> sql.VarBinary; sql.Table -> sql.TVP
+    var paramsV = {'sid': req.azureMobile.user.id };
+
+	var connection = new sql.Connection(util.sqlConfiguration);
+    connection.connect(function(err) {
+        var sqlRequest = new sql.Request(connection);
+        var preparedStatement = new sql.PreparedStatement(connection);
+        for (var key in paramsT) preparedStatement.input(key, paramsT[key]);
+        preparedStatement.prepare(selectedQuery, function(err) {
+            if (err) { completeError(err, res); return; }
+            preparedStatement.execute(paramsV, 
+                function(err, recordset, affected) {
+                    if (err) { completeError(err, res); return; }
+                    console.log(recordset);
+                    preparedStatement.unprepare();
+                    res.type('application/json').json(recordset);
+                }
+            );
+        });
+    });
 });
 
 router.get('/:id', function(req, res) {
-	var id = req.params.id;
+	var tr_id = req.params.id;
 	res.status(statusOK)
-		.send('Tertulia ' + id + ' details.');
+		.send('Tertulia ' + tr_id + ' details.');
 });
 
 router.get('/:id/defaultlocation', function(req, res) {
-	var id = 0;
+	var tr_id = req.params.id;
 	res.status(statusOK)
-		.send('Tertulia ' + id + ' default location.');
+		.send('Tertulia ' + tr_id + ' default location.');
 });
 
 router.get('/:id/locations', function(req, res) {
-	var id = 0;
+	var tr_id = req.params.id;
 	res.status(statusOK)
-		.send('Tertulia ' + id + ' locations list.');
+		.send('Tertulia ' + tr_id + ' locations list.');
 });
 
 router.get('/:id/members', function(req, res) {
-	var id = 0;
+	var tr_id = req.params.id;
 	res.status(statusOK)
-		.send('Tertulia ' + id + ' members list.');
+		.send('Tertulia ' + tr_id + ' members list.');
 });
 
 router.get('/:id/owner', function(req, res) {
-	var id = 0;
+	var tr_id = req.params.id;
 	res.status(statusOK)
-		.send('Tertulia ' + id + ' owner.');
+		.send('Tertulia ' + tr_id + ' owner.');
 });
 
 router.get('/:id/events', function(req, res) {
-	var id = 0;
+	var tr_id = req.params.id;
 	res.status(statusOK)
-		.send('Tertulia ' + id + ' events list.');
+		.send('Tertulia ' + tr_id + ' events list.');
 });
 
 router.get('/:id/nextevent', function(req, res) {
-	var id = 0;
+	var tr_id = req.params.id;
 	res.status(statusOK)
-		.send('Tertulia ' + id + ' next event.');
+		.send('Tertulia ' + tr_id + ' next event.');
 });
 
 module.exports = router;
