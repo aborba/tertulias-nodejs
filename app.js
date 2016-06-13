cb97764eb2bebc15352dba12f4c7115d7fbbf03f
Antonio Borba da Silva <abs@ggl.pt>
2016-06-13 11:47:03 +0100

    x

--- app.js
+++ app.js
@@ -114,10 +114,7 @@ mobile.tables
 .then(function () {
     //util.dumpObj(mobile);
     console.log('Registering the Azure Mobile Apps middleware.');
-    app.register = function (api) {
-        console.log('Registering');
-        api.get('calculator', getImplementation2);
-    };
+    app.route('calculator').get('calculator', getImplementation2);
     app.use(mobile);
     console.log('Listening for requests.');
     app.listen(process.env.PORT || 3000);
