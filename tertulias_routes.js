f1b8834670ce9c6d1d9ecf7fd3fc4d7f0d84023f
Antonio Borba da Silva <abs@ggl.pt>
2016-06-13 12:36:40 +0100

    x

--- tertulias_routes.js
+++ tertulias_routes.js
@@ -1,21 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
+var statusOK = 200;
+
 router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
+	console.log('Time: ', Date.now());
+	next();
 });
 
 router.get('/', function(req, res) {
-  res.send('Tertulias list');
+	res.status(statusOK)
+		.send('Tertulias list');
 });
 
 router.get('/:id', function(req, res) {
-  res.send('Tertulias item ');
+	var id = 0;
+	res.status(statusOK)
+		.send('Tertulia ' + id + ' details.');
 });
 
-router.get('/about', function(req, res) {
-  res.send('About Tertulias');
+router.get('/:id/defaultlocation', function(req, res) {
+	var id = 0;
+	res.status(statusOK)
+		.send('Tertulia ' + id + ' default location.');
+});
+
+router.get('/:id/locations', function(req, res) {
+	var id = 0;
+	res.status(statusOK)
+		.send('Tertulia ' + id + ' locations list.');
+});
+
+router.get('/:id/members', function(req, res) {
+	var id = 0;
+	res.status(statusOK)
+		.send('Tertulia ' + id + ' members list.');
+});
+
+router.get('/:id/owner', function(req, res) {
+	var id = 0;
+	res.status(statusOK)
+		.send('Tertulia ' + id + ' owner.');
+});
+
+router.get('/:id/events', function(req, res) {
+	var id = 0;
+	res.status(statusOK)
+		.send('Tertulia ' + id + ' events list.');
+});
+
+router.get('/:id/nextevent', function(req, res) {
+	var id = 0;
+	res.status(statusOK)
+		.send('Tertulia ' + id + ' next event.');
 });
 
 module.exports = router;
