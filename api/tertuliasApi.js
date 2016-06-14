13db7a27ef09dec12697dcaf928937cdf500a5e3
Antonio Borba da Silva <abs@ggl.pt>
2016-06-14 18:49:20 +0100

    x

--- api/tertuliasApi.js
+++ api/tertuliasApi.js
@@ -55,7 +55,9 @@ module.exports = function (configuration) {
 	                    	if (typeof req.t_links !== typeof undefined)
 	                			for (var key in req.t_links)
 	                				elem['_links'].key = { href : 'tertulias/' + elem.tr_id + '/' + req.t_links[key]};
+	                    	}
 	                    });
+	                    console.log(recordset);
 	                    preparedStatement.unprepare();
 	                    res.type('application/json').json(recordset);
 	                    next();
