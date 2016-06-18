6f11a1265c6083d9c674c90ff4a5e2149076ef3a
Antonio Borba da Silva <abs@ggl.pt>
2016-06-18 22:35:43 +0100

    x

--- api/tertuliasApi.js
+++ api/tertuliasApi.js
@@ -166,6 +166,7 @@ GO
 	    var paramsV = req.paramsV;
 
 		var connection = new sql.Connection(util.sqlConfiguration);
+		console.log(connection);
 	    connection.beginTransaction(function(err) {
 	    	var SQL = 'SELECT nv_id FROM EnumTypes INNER JOIN EnumValues ON nv_type = nt_id '+
 	    	'WHERE nt_name = @enumtype AND nv_name = @name'
@@ -184,7 +185,7 @@ GO
 
 	    	connection.rollback();
 	    	return;
-	    	
+
 	        for (var key in paramsT) preparedStatement.input(key, paramsT[key]);
 	        preparedStatement.prepare(selectedQuery, function(err) {
 	            if (err) { completeError(err, res); return; }
