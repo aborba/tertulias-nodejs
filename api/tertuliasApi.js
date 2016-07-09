67bf211090d89eeee509df028a9425a7b6073154
Antonio Borba da Silva <abs@ggl.pt>
2016-07-09 18:58:51 +0100

    wip save

--- api/tertuliasApi.js
+++ api/tertuliasApi.js
@@ -200,15 +200,16 @@ module.exports = function (configuration) {
 
 	router.post('/:tr_id/subscribe', (req, res, next) => {
 		console.log('in /:tr_id/subscribe');
-
 	    sql.connect(util.sqlConfiguration)
 	    .then(function() {
-			new sql.query('SELECT mb_id FROM Members' +
+			new sql.Request()
+			.query('SELECT mb_id FROM Members' +
 				' INNER JOIN Tertulias ON mb_tertulia = tr_id' +
 				' INNER JOIN Users ON mb_user = us_id' +
 				' WHERE tr_cancelled = 0')
 			.then((recordset) => {
 				console.log(recordset);
+				return next();
 			})
 			.catch(function(err) {
 				console.log('catch1');
