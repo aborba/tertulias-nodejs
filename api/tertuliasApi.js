2f61bacaac08e3e750e0dab6c4c9dfd4f1d37d37
Antonio Borba da Silva <abs@ggl.pt>
2016-07-09 19:25:36 +0100

    wip save

--- api/tertuliasApi.js
+++ api/tertuliasApi.js
@@ -204,10 +204,11 @@ module.exports = function (configuration) {
 	    .then(function() {
 			new sql.Request()
 			.input('sid', sql.NVarChar, req.azureMobile.user.id)
-			.query('SELECT COUNT(mb_id) FROM Members' +
+			.input('tr_id', sql.Int, req.params.tr_id)
+			.query('SELECT * WHERE @tr_id IN (SELECT mb_id FROM Members' +
 				' INNER JOIN Tertulias ON mb_tertulia = tr_id' +
 				' INNER JOIN Users ON mb_user = us_id' +
-				' WHERE tr_is_cancelled = 0 AND us_sid <> @sid')
+				' WHERE tr_is_cancelled = 0 AND us_sid <> @sid)')
 			.then((recordset) => {
 				console.log(recordset);
 				return next();
