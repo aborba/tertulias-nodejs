fb5f96ca88b05733f93fadda0ec0ecf489538ea2
Antonio Borba da Silva <abs@ggl.pt>
2016-07-10 23:12:50 +0100

    wip save

--- api/tertuliasApi.js
+++ api/tertuliasApi.js
@@ -193,7 +193,7 @@ module.exports = function (configuration) {
 					']';
                 res.type('application/json');
                 var results = {};
-            	results['tertulias'] = recordset;
+            	results['tertulia'] = recordset[0];
                 results['links'] = JSON.parse(links);
 	    		console.log(results);
                 res.json(results);
