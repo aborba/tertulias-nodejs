dabdbe34b1eb54af060a8ff919d8dd15a426f3b1
Antonio Borba da Silva <abs@ggl.pt>
2016-08-19 09:32:19 +0100

    wip save

---
+++ api/registration.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+	bodyParser = require('body-parser'),
+    authenticate = require('azure-mobile-apps/src/express/middleware/authenticate'),
+    authorize = require('azure-mobile-apps/src/express/middleware/authorize');
+
+var util = require('../util');
+
+module.exports = function (configuration) {
+    var router = express.Router();
+
+	var completeError = function(err, res) {
+	    if (err) {
+	        console.error(err);
+	        if (res) res.sendStatus(500);
+	    }
+	};
+
+    router.get('/', (req, res, next) => {
+		res.send( 'Hello world' );
+    	next();
+    });
+
+    return router;
+};
