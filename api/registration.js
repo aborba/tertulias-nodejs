a21f6164eb509cb8c22e52832b75eb8aff8345e1
Antonio Borba da Silva <abs@ggl.pt>
2016-08-19 13:39:13 +0100

    wip save

--- api/registration.js
+++ api/registration.js
@@ -19,7 +19,7 @@ module.exports = function (configuration) {
     router.get('/:voucher', (req, res, next) => {
 		console.log('in GET /private_invitation/:voucher');
 		var voucher = req.params.voucher;
-		var userSid = req.azureMobile.user.id;
+		var userSid = 'req.azureMobile.user.id';
 		var body = '<h1>Tertulias</h1>\n' +
 			'	<p>Welcome to Tertulias platform site.</p>' +
 			'	<p>You arrived at this page because you followed a link with a private invitation from a friend of yours to join a Tertulia.</p>' +
