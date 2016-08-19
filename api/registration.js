5d166dc85c75d468440dd5982a5432b795c37419
Antonio Borba da Silva <abs@ggl.pt>
2016-08-19 10:56:49 +0100

    wip save

--- api/registration.js
+++ api/registration.js
@@ -1,9 +1,9 @@
-var express = require('express'),
-	bodyParser = require('body-parser'),
+var express      = require('express'),
+	bodyParser   = require('body-parser'),
     authenticate = require('azure-mobile-apps/src/express/middleware/authenticate'),
-    authorize = require('azure-mobile-apps/src/express/middleware/authorize');
-
-var util = require('../util');
+    authorize    = require('azure-mobile-apps/src/express/middleware/authorize'),
+	sql          = require('mssql'),
+	util         = require('../util');
 
 module.exports = function (configuration) {
     var router = express.Router();
@@ -15,11 +15,13 @@ module.exports = function (configuration) {
 	    }
 	};
 
-    router.get('/', (req, res, next) => {
-		console.log('in GET /private_invitation');
+    router.get('/:voucher', (req, res, next) => {
+		console.log('in GET /private_invitation/:voucher');
+		var voucher = req.params.voucher;
 		var body = '<h1>Tertulias</h1>\n' +
 			'<p>Welcome to Tertulias site.</p>' +
 			'<p>You arrived at this page because you followed a link with a private invitation from a friend of yours to join a Tertulia.</p>' +
+			'<p>Your voucher number is ' + voucher + '.</p>' +
 			'';
 		res.send(body);
     	next();
