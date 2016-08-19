471cc20521b790223db9a218dd37b7ba23c7c371
Antonio Borba da Silva <abs@ggl.pt>
2016-08-19 23:54:42 +0100

    wip save

--- api/registration.js
+++ api/registration.js
@@ -20,13 +20,13 @@ module.exports = function (configuration) {
 		console.log('in GET /private_invitation/:voucher');
 		if (! req.azureMobile.user) {
 			console.log('redirecting...');
-			// res.writeHead(302, {
-			// 	'Location': 'tertulias.azurewebsites.net/.auth/login/google',
-			// 	'redirect_uri': 'https://tertulias.azurewebsites.net/.auth/login/google/callback'
-			// });
+			res.writeHead(302, {
+				'Location': 'tertulias.azurewebsites.net/.auth/login/google',
+				'redirect_uri': 'https://tertulias.azurewebsites.net/.auth/login/google/callback'
+			});
 
-			res.redirect('tertulias.azurewebsites.net/.auth/login/google?redirect_uri=' +
-				encodeURI('https://tertulias.azurewebsites.net/.auth/login/google/callback'));
+			// res.redirect('tertulias.azurewebsites.net/.auth/login/google?redirect_uri=' +
+			// 	encodeURI('https://tertulias.azurewebsites.net/.auth/login/google/callback'));
 			res.end();
 			return;
 		}
