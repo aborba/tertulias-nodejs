7139bb9aa729e53e9c8c2dda0ec09593a95a570b
Antonio Borba da Silva <abs@ggl.pt>
2016-08-20 17:34:16 +0100

    wip save

--- api/registation.js
+++ api/registation.js
@@ -36,10 +36,7 @@ module.exports      = function (configuration) {
     		'			.done(\n' +
     		'				function(results) {\n' +
     		'					var href = window.location.href;\n' +
-    		'					console.log(href);\n' +
-    		'					var pos = href.lastIndexOf("/");\n' +
-    		'					console.log(pos);\n' +
-    		'					alert(href.substr(10 + 1));\n' +
+    		'					alert(href.substr(href.lastIndexOf("/") + 1));\n' +
     		'					var userSid = results.userId;\n' +
     		'					var url = window.location.href;\n' +
     		'					//var voucher = document.getElementById("voucher").innerHTML;\n' +
