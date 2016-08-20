657e8a1762939eabcd2ee33dcdf9c3230142b5d2
Antonio Borba da Silva <abs@ggl.pt>
2016-08-20 16:06:17 +0100

    wip save

--- api/registation.js
+++ api/registation.js
@@ -29,13 +29,19 @@ module.exports      = function (configuration) {
 			'	<p>Welcome to Tertulias platform site.</p>' +
 			'	<p>You arrived at this page because you followed a link with a private invitation from a friend of yours to join a Tertulia.</p>' +
 			'	<p>Your voucher number is <strong>' + voucher + '</strong>.</p>' +
-			'	<p>Your user id is <strong>' + userSid + '</strong>.</p>' +
+			'	<p>Your user id is <strong><span id="userId"></span></strong>.</p>' +
 			'<script>' +
 			'	function signIn(){' +
      		'		var MobileServiceClient = WindowsAzure.MobileServiceClient;' +
      		'		var client = new MobileServiceClient("https://tertulias.azurewebsites.net", "309180942544-p7pg44n9uamccukt8caic0jerl2jpmta.apps.googleusercontent.com");' +
     		'		client.login("google").done(' +
-    		'			function (results) { alert("You are now logged in as: " + results.userId); },' +
+    		'			function (results) {' +
+    		'				alert("You are now logged in as: " + results.userId);' +
+    		'				document.getElementById("userId").innerHTML = results.userId;' +
+    		'' +
+    		'' +
+    		'' +
+    		'			},' +
     		'			function (err) { alert("Error: " + err); });' +
 			'	}' +
 			'	alert("Hello");' +
