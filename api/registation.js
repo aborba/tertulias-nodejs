896d53258ce70c9d629af3dc91a7798974b7b36c
Antonio Borba da Silva <abs@ggl.pt>
2016-08-20 17:39:17 +0100

    wip save

--- api/registation.js
+++ api/registation.js
@@ -26,8 +26,8 @@ module.exports      = function (configuration) {
 			'<h1>Tertulias</h1>\n' +
 			'	<p>Welcome to Tertulias platform site.</p>\n' +
 			'	<p>You arrived at this page because you followed a link with a private invitation from a friend of yours to join a Tertulia.</p>\n' +
-			'	<p>Your voucher number is <strong><span id="voucher">' + voucher + '</span></strong>.</p>\n' +
-			'	<p>Your user id is <strong><span id="userId">________________________________</span></strong>.</p>\n' +
+			'	<p>Your voucher number is <strong><span id="voucher">____________________________________</span></strong>.</p>\n' +
+			'	<p>Your user id is <strong><span id="userId">____________________________________</span></strong>.</p>\n' +
 			'<script>\n' +
 			'	function signIn() {\n' +
      		'		new WindowsAzure.MobileServiceClient("https://tertulias.azurewebsites.net",\n' +
@@ -36,10 +36,10 @@ module.exports      = function (configuration) {
     		'			.done(\n' +
     		'				function(results) {\n' +
     		'					var href = window.location.href;\n' +
-    		'					var userSid = results.userId;\n' +
     		'					var voucher = href.substr(href.lastIndexOf("/") + 1);\n' +
+    		'					var userSid = results.userId;\n' +
+    		'					document.getElementById("voucher").innerHTML = voucher;\n' +
     		'					document.getElementById("userId").innerHTML = userSid;\n' +
-    		'					alert("userId: " + userSid + ", voucher: " + voucher);\n' +
     		'				},\n' +
     		'				function(err) { alert("Error: " + err); }\n' +
     		'			);\n' +
