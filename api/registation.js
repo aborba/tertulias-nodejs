e4ac0ab5567bc267dee2c0ebcd67a003ffc7bbf8
Antonio Borba da Silva <abs@ggl.pt>
2016-08-20 20:37:11 +0100

    wip save

--- api/registation.js
+++ api/registation.js
@@ -5,18 +5,18 @@ var sql             = require('mssql'),
 	util            = require('../util');
 
 module.exports      = function (configuration) {
-    var router      = express.Router(),
-    authenticate    = require('azure-mobile-apps/src/express/middleware/authenticate')(configuration),
-    authorize       = require('azure-mobile-apps/src/express/middleware/authorize');
+	var router      = express.Router(),
+	authenticate    = require('azure-mobile-apps/src/express/middleware/authenticate')(configuration),
+	authorize       = require('azure-mobile-apps/src/express/middleware/authorize');
 
 	var completeError = function(err, res) {
-	    if (err) {
-	        console.error(err);
-	        if (res) res.sendStatus(500);
-	    }
+		if (err) {
+			console.error(err);
+			if (res) res.sendStatus(500);
+		}
 	};
 
-    router.get('/:voucher', authenticate, (req, res, next) => {
+	router.get('/:voucher', authenticate, (req, res, next) => {
 		console.log('in GET /private_invitation/:voucher');
 		console.log(configuration);
 		var voucher = req.params.voucher;
@@ -28,27 +28,36 @@ module.exports      = function (configuration) {
 			'	<p>You arrived at this page because you followed a link with a private invitation from a friend of yours to join a Tertulia.</p>\n' +
 			'	<p>Your voucher number is <strong><span id="voucher">____________________________________</span></strong>.</p>\n' +
 			'	<p>Your user id is <strong><span id="userId">____________________________________</span></strong>.</p>\n' +
-			'<script type="text/javascript">\n' +
-			'	function signIn() {\n' +
-     		'		new WindowsAzure.MobileServiceClient("https://tertulias.azurewebsites.net",\n' +
-     		'				"309180942544-p7pg44n9uamccukt8caic0jerl2jpmta.apps.googleusercontent.com")\n' +
-    		'			.login("google")\n' +
-    		'			.done(\n' +
-    		'				function(results) {\n' +
-    		'					var href = window.location.href;\n' +
-    		'					var voucher = href.substr(href.lastIndexOf("/") + 1);\n' +
-    		'					var userSid = results.userId;\n' +
-    		'					document.getElementById("voucher").innerHTML = voucher;\n' +
-    		'					document.getElementById("userId").innerHTML = userSid;\n' +
-    		'				},\n' +
-    		'				function(err) { alert("Error: " + err); }\n' +
-    		'			);\n' +
+			'<script type="application/javascript">\n' +
+			'	function signIn(subscribe) {\n' +
+			'		new WindowsAzure.MobileServiceClient("https://tertulias.azurewebsites.net",\n' +
+			'				"309180942544-p7pg44n9uamccukt8caic0jerl2jpmta.apps.googleusercontent.com")\n' +
+			'			.login("google")\n' +
+			'			.done(\n' +
+			'				function(results) {\n' +
+			'					var href = window.location.href;\n' +
+			'					var voucher = href.substr(href.lastIndexOf("/") + 1);\n' +
+			'					var userSid = results.userId;\n' +
+			'					document.getElementById("voucher").innerHTML = voucher;\n' +
+			'					document.getElementById("userId").innerHTML = userSid;\n' +
+			'					subscribe(userSid, voucher);\n' +
+			'				},\n' +
+			'				function(err) { alert("Error: " + err); }\n' +
+			'			);\n' +
 			'	};\n' +
-			'	signIn();\n' +
+			'	function subscribe(userSid, voucher) {\n' +
+			'		alert(userId + " " + voucher);' +
+			'		' +
+			'		' +
+			'		' +
+			'	};\n' +
+			'</script>\n' +
+			'<script type="application/javascript">' +
+			'	signIn(subscribe);\n' +
 			'</script>\n' +
 			'';
 		res.send(body);
-    	next();
-    });
-    return router;
+		next();
+	});
+	return router;
 };
