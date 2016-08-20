376f08466a79d89fb26fdff04075e6abfb9b538e
Antonio Borba da Silva <abs@ggl.pt>
2016-08-20 14:10:48 +0100

    wip save

--- api/registation.js
+++ api/registation.js
@@ -16,7 +16,7 @@ module.exports = function (configuration) {
 	    }
 	};
 
-    router.get('/:voucher', authenticate, (req, res, next) => {
+    router.get('/:voucher', (req, res, next) => {
 		console.log('in GET /private_invitation/:voucher');
 		var voucher = req.params.voucher;
 		var userSid = 'req.azureMobile.user.id';
