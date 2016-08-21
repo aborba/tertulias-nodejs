76378427060f20d6b8621f940d343757a9f94b7c
Antonio Borba da Silva <abs@ggl.pt>
2016-08-21 13:09:54 +0100

    wip save

--- api/tertuliasApi.js
+++ api/tertuliasApi.js
@@ -1,25 +1,25 @@
 var express      = require('express'),
 	bodyParser   = require('body-parser'),
-    authenticate = require('azure-mobile-apps/src/express/middleware/authenticate'),
-    authorize    = require('azure-mobile-apps/src/express/middleware/authorize');
+	authenticate = require('azure-mobile-apps/src/express/middleware/authenticate'),
+	authorize    = require('azure-mobile-apps/src/express/middleware/authorize');
 
 var sql          = require('mssql'),
-    util         = require('../util');
+	util         = require('../util');
 
 /* { 'SQL types': {
 	'String': 'sql.NVarChar', 'Number': 'sql.Int', 'Boolean': 'sql.Bit', 'Date': 'sql.DateTime', 'Buffer': 'sql.VarBinary', 'sql.Table': 'sql.TVP'
 } } */
 module.exports = function (configuration) {
-    var router = express.Router();
+	var router = express.Router();
 
-    router.get('/', (req, res, next) => {
+	router.get('/', (req, res, next) => {
 		console.log('in GET /api/tertulias');
 		var route = '/tertulias';
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
-	    	.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
-	    	.query('SELECT' +
+			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
+			.query('SELECT' +
 					' tr_id         AS id,' +
 					' tr_name       AS name,' +
 					' tr_subject    AS subject,' +
@@ -41,8 +41,8 @@ module.exports = function (configuration) {
 						' INNER JOIN Users ON rn_user = us_id WHERE us_sid = @userSid)' +
 				' GROUP BY no_tertulia) AS c ON no_tertulia = tr_id' +
 				' WHERE tr_is_cancelled = 0 AND us_sid = @userSid')
-	    	.then(function(recordset) {
-			    var links = '[ ' +
+			.then(function(recordset) {
+				var links = '[ ' +
 						'{ "rel": "self", "method": "GET", "href": "' + route + '" }, ' +
 						'{ "rel": "create_weekly", "method": "POST", "href": "' + route + '/weekly" }, ' +
 						'{ "rel": "create_monthly", "method": "POST", "href": "' + route + '/monthly" }, ' +
@@ -51,7 +51,7 @@ module.exports = function (configuration) {
 						'{ "rel": "create_yearlyw", "method": "POST", "href": "' + route + '/yearlyw" }, ' +
 						'{ "rel": "searchPublic", "method": "GET", "href": "' + route + '/publicsearch" } ' +
 					']';
-			    var itemLinks = '[ ' +
+				var itemLinks = '[ ' +
 						'{ "rel": "self", "method": "GET", "href": "' + route + '/:id" }, ' +
 						'{ "rel": "update", "method": "PATCH", "href": "' + route + '/:id" }, ' +
 						'{ "rel": "delete", "method": "DELETE", "href": "' + route + '/:id" }, ' +
@@ -60,102 +60,102 @@ module.exports = function (configuration) {
 						'{ "rel": "event", "method": "POST", "href": "' + route + '/:id/event" } ' +
 					']';
 				res.type('application/json');
-                recordset.forEach(function(elem) {
-                	elem['links'] = JSON.parse(itemLinks.replace(/:id/g, elem.id));
-        		});
-                var results = {};
-            	results['tertulias'] = recordset;
-                results['links'] = JSON.parse(links);
-                res.json(results);
-                res.sendStatus(200);
-                return next();
-	    	})
-	    });
-    });
+				recordset.forEach(function(elem) {
+					elem['links'] = JSON.parse(itemLinks.replace(/:id/g, elem.id));
+				});
+				var results = {};
+				results['tertulias'] = recordset;
+				results['links'] = JSON.parse(links);
+				res.json(results);
+				res.sendStatus(200);
+				return next();
+			})
+		});
+	});
 
-    router.get('/voucherinfo/:voucher', (req, res, next) => {
+	router.get('/voucherinfo/:voucher', (req, res, next) => {
 		console.log('in GET /api/voucherinfo/:voucher');
 		var voucher = req.params.voucher;
 		console.log(voucher);
 		console.log(req.azureMobile.user);
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
-	    	.input('voucher', sql.VarChar(40), voucher)
-	    	.query('SELECT' +
-		    		' tr_name AS name,' +
-		    		' tr_subject AS subject' +
-	    		' FROM Invitations' +
-	    			' INNER JOIN Tertulias ON in_tertulia = tr_id' +
-	    		' WHERE tr_is_cancelled = 0' +
-	    			' AND in_is_acknowledged = 0 AND in_key = @voucher')
-	    	.then(function(recordset) {
-	    		concole.log(recordset);
-                var links = '[ { "rel": "self", "method": "GET", "href": "' + route + '/publicSearch" } ]';
-                res.type('application/json');
-                var results = {};
-            	results['tertulias'] = recordset;
-                results['links'] = JSON.parse(links);
-                res.json(results);
-                res.sendStatus(200);
-                return next();
-            })
-	    });
+			.input('voucher', sql.VarChar(40), voucher)
+			.query('SELECT' +
+					' tr_name AS name,' +
+					' tr_subject AS subject' +
+				' FROM Invitations' +
+					' INNER JOIN Tertulias ON in_tertulia = tr_id' +
+				' WHERE tr_is_cancelled = 0' +
+					' AND in_is_acknowledged = 0 AND in_key = @voucher')
+			.then(function(recordset) {
+				console.log(recordset);
+				var links = '[ { "rel": "self", "method": "GET", "href": "' + route + '/publicSearch" } ]';
+				res.type('application/json');
+				var results = {};
+				results['tertulias'] = recordset;
+				results['links'] = JSON.parse(links);
+				res.json(results);
+				res.sendStatus(200);
+				return next();
+			})
+		});
 	});
 
-    router.get('/publicSearch', (req, res, next) => {
+	router.get('/publicSearch', (req, res, next) => {
 		console.log('in GET /tertulias/publicsearch');
 		var route = '/tertulias';
 		var point = 'POINT(' + req.query.latitude + ' ' + req.query.longitude + ')';
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
-	    	.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
-	    	.input('query', sql.NVarChar, '%' + req.query.query + '%')
-	    	.input('latitude', sql.NVarChar, req.query.latitude)
-	    	.input('longitude', sql.NVarChar, req.query.longitude)
-	    	.input('point', sql.NVarChar, point)
-	    	.query('SELECT TOP 25' +
-		    		' tr_id AS id,' +
-		    		' tr_name AS name,' +
-		    		' tr_subject AS subject,' +
-		    		' lo_name AS location' +
-	    		' FROM Tertulias' +
-	    			' INNER JOIN Locations ON tr_location = lo_id' +
-	    		' WHERE (tr_name LIKE @query OR tr_subject LIKE @query OR lo_name LIKE @query)' +
-	    			' AND tr_is_cancelled = 0 AND tr_is_private = 0' +
-		    		' AND tr_id NOT IN' +
-		    			' (SELECT mb_tertulia FROM Tertulias' +
-		    			' INNER JOIN Members ON mb_tertulia = tr_id' +
-		    			' INNER JOIN Users ON mb_user = us_id WHERE us_sid = @userSid)' +
+			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
+			.input('query', sql.NVarChar, '%' + req.query.query + '%')
+			.input('latitude', sql.NVarChar, req.query.latitude)
+			.input('longitude', sql.NVarChar, req.query.longitude)
+			.input('point', sql.NVarChar, point)
+			.query('SELECT TOP 25' +
+					' tr_id AS id,' +
+					' tr_name AS name,' +
+					' tr_subject AS subject,' +
+					' lo_name AS location' +
+				' FROM Tertulias' +
+					' INNER JOIN Locations ON tr_location = lo_id' +
+				' WHERE (tr_name LIKE @query OR tr_subject LIKE @query OR lo_name LIKE @query)' +
+					' AND tr_is_cancelled = 0 AND tr_is_private = 0' +
+					' AND tr_id NOT IN' +
+						' (SELECT mb_tertulia FROM Tertulias' +
+						' INNER JOIN Members ON mb_tertulia = tr_id' +
+						' INNER JOIN Users ON mb_user = us_id WHERE us_sid = @userSid)' +
 				' ORDER BY lo_geography.STDistance( @point )')
 				// ' ORDER BY lo_geography.STDistance(\'POINT( @latitude @longitude )\')')
 				// ' ORDER BY lo_geography.STDistance(\'POINT( 38.11 -9.1123113 )\')')
-	    	.then(function(recordset) {
-                var links = '[ { "rel": "self", "method": "GET", "href": "' + route + '/publicSearch" } ]';
-                var itemLinks = '[ ' +
-            	    	'{ "rel": "self", "method": "GET", "href": "' + route + '/:id" }, ' +
+			.then(function(recordset) {
+				var links = '[ { "rel": "self", "method": "GET", "href": "' + route + '/publicSearch" } ]';
+				var itemLinks = '[ ' +
+						'{ "rel": "self", "method": "GET", "href": "' + route + '/:id" }, ' +
 						'{ "rel": "subscribe", "method": "POST", "href": "' + route + '/:id/subscribe" }' +
 					']';
-                res.type('application/json');
-                recordset.forEach(function(elem) {
-                	elem['links'] = JSON.parse(itemLinks.replace(/:id/g, elem.id));
-        		});
-                var results = {};
-            	results['tertulias'] = recordset;
-                results['links'] = JSON.parse(links);
-                res.json(results);
-                res.sendStatus(200);
-                return next();
-            })
-	    });
+				res.type('application/json');
+				recordset.forEach(function(elem) {
+					elem['links'] = JSON.parse(itemLinks.replace(/:id/g, elem.id));
+				});
+				var results = {};
+				results['tertulias'] = recordset;
+				results['links'] = JSON.parse(links);
+				res.json(results);
+				res.sendStatus(200);
+				return next();
+			})
+		});
 	});
 
 	router.post('/weekly', (req, res, next) => {
 		console.log('in POST /tertulias/weekly');
 		console.log(req.body);
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
 			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 			.input('tertuliaName', sql.NVarChar(40), req.body.tertulia_name)
@@ -198,8 +198,8 @@ module.exports = function (configuration) {
 	router.post('/monthly', (req, res, next) => {
 		console.log('in POST /tertulias/monthly');
 		console.log(req.body);
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
 			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 			.input('tertuliaName', sql.NVarChar(40), req.body.tertulia_name)
@@ -242,8 +242,8 @@ module.exports = function (configuration) {
 	router.post('/monthlyw', (req, res, next) => {
 		console.log('in POST /tertulias/monthlyw');
 		console.log(req.body);
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
 			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 			.input('tertuliaName', sql.NVarChar(40), req.body.tertulia_name)
@@ -291,8 +291,8 @@ module.exports = function (configuration) {
 		var route = '/tertulias/' + tr_id;
 		if (isNaN(tr_id))
 			return next();
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
 			.input('tertulia', sql.Int, tr_id)
 			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
@@ -330,8 +330,8 @@ module.exports = function (configuration) {
 						' WHERE us_sid = @userSid)))' +
 					' AND tr_id = @tertulia')
 			.then(function(recordset) {
-                var results = {};
-            	results['tertulia'] = recordset[0];
+				var results = {};
+				results['tertulia'] = recordset[0];
 				var links = '[ ' +
 						'{ "rel": "self", "method": "GET", "href": "' + route + '" }, ' +
 						'{ "rel": "update", "method": "PATCH", "href": "' + route + '" }, ' +
@@ -340,14 +340,14 @@ module.exports = function (configuration) {
 						'{ "rel": "subscribe", "method": "POST", "href": "' + route + '/subscribe" }, ' +
 						'{ "rel": "unsubscribe", "method": "DELETE", "href": "' + route + '/unsubscribe" } ' +
 					']';
-                results['links'] = JSON.parse(links);
+				results['links'] = JSON.parse(links);
 				req.results = results;
 				console.log("Schedule name: " + results['tertulia'].schedule_name);
 				switch(results['tertulia'].schedule_name.toUpperCase()) {
 					case 'WEEKLY':
 						console.log('in weekly');
-					    sql.connect(util.sqlConfiguration)
-					    .then(function() {
+						sql.connect(util.sqlConfiguration)
+						.then(function() {
 							new sql.Request()
 							.input('schedule', sql.Int, recordset[0].schedule_id)
 							// .input('tertulia', sql.Int, recordset[0].tertulia_id)
@@ -364,17 +364,17 @@ module.exports = function (configuration) {
 							.then(function(recordset) {
 								results['weekly'] = recordset[0];
 								console.log(results);
-				                res.type('application/json');
-				                res.json(results);
-				                res.sendStatus(200);
-				                return next();
+								res.type('application/json');
+								res.json(results);
+								res.sendStatus(200);
+								return next();
 							})
 						});
 						break;
 					case 'MONTHLYD':
 						console.log('in monthly');
-					    sql.connect(util.sqlConfiguration)
-					    .then(function() {
+						sql.connect(util.sqlConfiguration)
+						.then(function() {
 							new sql.Request()
 							.input('schedule', sql.Int, recordset[0].schedule_id)
 							// .input('tertulia', sql.Int, recordset[0].tertulia_id)
@@ -391,17 +391,17 @@ module.exports = function (configuration) {
 							.then(function(recordset) {
 								results['monthly'] = recordset[0];
 								console.log(results);
-				                res.type('application/json');
-				                res.json(results);
-				                res.sendStatus(200);
-				                return next();
+								res.type('application/json');
+								res.json(results);
+								res.sendStatus(200);
+								return next();
 							})
 						});
 						break;
 					case 'MONTHLYW':
 						console.log('in monthlyw');
-					    sql.connect(util.sqlConfiguration)
-					    .then(function() {
+						sql.connect(util.sqlConfiguration)
+						.then(function() {
 							new sql.Request()
 							.input('schedule', sql.Int, recordset[0].schedule_id)
 							//.input('tertulia', sql.Int, recordset[0].tertulia_id)
@@ -420,10 +420,10 @@ module.exports = function (configuration) {
 							.then(function(recordset) {
 								results['monthlyw'] = recordset[0];
 								console.log(results);
-				                res.type('application/json');
-				                res.json(results);
-				                res.sendStatus(200);
-				                return next();
+								res.type('application/json');
+								res.json(results);
+								res.sendStatus(200);
+								return next();
 							})
 						});
 						break;
@@ -449,8 +449,8 @@ module.exports = function (configuration) {
 		switch (req.body.schedule_name.toUpperCase()) {
 			case "WEEKLY":
 				console.log("in WEEKLY");
-			    sql.connect(util.sqlConfiguration)
-			    .then(function() {
+				sql.connect(util.sqlConfiguration)
+				.then(function() {
 					new sql.Request()
 					.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 					.input('tertuliaId', sql.Int, tr_id)
@@ -494,8 +494,8 @@ module.exports = function (configuration) {
 				break;
 			case "MONTHLYD":
 				console.log("in MONTHLYD");
-			    sql.connect(util.sqlConfiguration)
-			    .then(function() {
+				sql.connect(util.sqlConfiguration)
+				.then(function() {
 					new sql.Request()
 					.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 					.input('tertuliaId', sql.Int, tr_id)
@@ -540,8 +540,8 @@ module.exports = function (configuration) {
 				break;
 			case "MONTHLYW":
 				console.log("in MONTHLYW");
-			    sql.connect(util.sqlConfiguration)
-			    .then(function() {
+				sql.connect(util.sqlConfiguration)
+				.then(function() {
 					new sql.Request()
 					.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 					.input('tertuliaId', sql.Int, tr_id)
@@ -594,38 +594,38 @@ module.exports = function (configuration) {
 		}
 	});
 
-    router.get('/:tr_id/members', (req, res, next) => {
+	router.get('/:tr_id/members', (req, res, next) => {
 		console.log('in GET /tertulias/:tr_id/members');
 		var tr_id = req.params.tr_id;
 		var tertulia = '/tertulias/' + tr_id;
 		var route = tertulia + '/members';
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
-	    	.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
-	    	.input('tertulia', sql.Int, tr_id)
+			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
+			.input('tertulia', sql.Int, tr_id)
 			.execute('sp_getTertuliaMembers')
-	    	.then(function(recordset) {
-                var links = '[ ' +
+			.then(function(recordset) {
+				var links = '[ ' +
 					'{ "rel": "self", "method": "GET", "href": "' + route + '" }, ' +
 					'{ "rel": "create_vouchers", "method": "POST", "href": "' + route + '/voucher" } ' +
-            	']';
-                var itemLinks = '[ ' +
-        	    	'{ "rel": "self", "method": "GET", "href": "' + route + '/:id" }, ' +
+				']';
+				var itemLinks = '[ ' +
+					'{ "rel": "self", "method": "GET", "href": "' + route + '/:id" }, ' +
 					'{ "rel": "edit_member", "method": "PATCH", "href": "' + route + '/:id/edit" }' +
 				']';
-                res.type('application/json');
-                recordset[0].forEach(function(elem) {
-                	elem['links'] = JSON.parse(itemLinks.replace(/:id/g, elem.id));
-        		});
-                var results = {};
-            	results['members'] = recordset[0];
-                results['links'] = JSON.parse(links);
-                res.json(results);
-                res.sendStatus(200);
-                return next();
-            })
-	    });
+				res.type('application/json');
+				recordset[0].forEach(function(elem) {
+					elem['links'] = JSON.parse(itemLinks.replace(/:id/g, elem.id));
+				});
+				var results = {};
+				results['members'] = recordset[0];
+				results['links'] = JSON.parse(links);
+				res.json(results);
+				res.sendStatus(200);
+				return next();
+			})
+		});
 	});
 
 	router.post('/:tr_id/members/voucher', (req, res, next) => {
@@ -645,7 +645,7 @@ module.exports = function (configuration) {
 				console.log(route);
 				var batch = request.parameters.vouchers_batch.value;
 				console.log(batch);
-			    var links = '[ ' +
+				var links = '[ ' +
 					// '{ "rel": "self", "method": "GET", "href": "' + route + '" }, ' +
 					'{ "rel": "get_vouchers", "method": "GET", "href": "' + route + '/' + batch + '" } ' +
 				']';
@@ -723,8 +723,8 @@ module.exports = function (configuration) {
 
 	router.post('/:tr_id/subscribe', (req, res, next) => {
 		console.log('in POST /tertulias/:tr_id/subscribe');
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
 			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 			.input('tertulia', sql.Int, req.params.tr_id)
@@ -748,8 +748,8 @@ module.exports = function (configuration) {
 
 	router.delete('/:tr_id/unsubscribe', (req, res, next) => {
 		console.log('in DELETE /tertulias/:tr_id/unsubscribe');
-	    sql.connect(util.sqlConfiguration)
-	    .then(function() {
+		sql.connect(util.sqlConfiguration)
+		.then(function() {
 			new sql.Request()
 			.input('userSid', sql.NVarChar(40), req.azureMobile.user.id)
 			.input('tertulia', sql.Int, req.params.tr_id)
@@ -773,13 +773,13 @@ module.exports = function (configuration) {
 	});
 
 	var completeError = function(err, res) {
-	    if (err) {
-	    	console.log("Error:");
-	        console.error(err);
-	        if (res) res.sendStatus(500);
-	    }
+		if (err) {
+			console.log("Error:");
+			console.error(err);
+			if (res) res.sendStatus(500);
+		}
 	};
 
-    return router;
+	return router;
 
 }
