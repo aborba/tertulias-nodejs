9e795d01df2da23a645ce0a790a7a1331d6513b4
Antonio Borba da Silva <abs@ggl.pt>
2016-08-21 01:46:49 +0100

    wip save

--- public/TertuliasBody.js
+++ public/TertuliasBody.js
@@ -1,13 +1,20 @@
 function getVoucher(href){return href.substr(href.lastIndexOf("/")+1);};
-function signInAndSubscribe(voucher, placeholder, message){
+function signInAndSubscribe(voucher, confirmationQuestion, placeholder, message){
 	new WindowsAzure.MobileServiceClient("https://tertulias.azurewebsites.net","309180942544-p7pg44n9uamccukt8caic0jerl2jpmta.apps.googleusercontent.com")
 	.login("google")
 	.done(function(results){
 			var userSid=results.userId;
 			var voucher=getVoucher(window.location.href);
 			document.getElementById(placeholder).innerHTML=message+" <strong>"+userSid+"</strong>.";
-			subscribe(userSid, voucher);},
+			areYouSure(confirmationQuestion, userSid, voucher);
+		},
 		function(err){alert("Error: "+err);});
+};
+function areYouSure(confirmationQuestion, userSid, voucher){
+	if (confirm(confirmationQuestion)) {
+		subscribe(userSid, voucher);
+		console.log('subscribing');
+	}
 };
 function subscribe(userSid,voucher){
 	document.getElementById("userId").innerHTML=userSid;
