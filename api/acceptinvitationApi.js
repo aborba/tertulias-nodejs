a8a0e8a8af07230df194c096915c748f1bf5ec92
Antonio Borba da Silva <abs@ggl.pt>
2016-08-21 14:22:39 +0100

    wip save

--- api/acceptinvitationApi.js
+++ api/acceptinvitationApi.js
@@ -5,24 +5,24 @@ var sql             = require('mssql'),
 	util            = require('../util');
 
 module.exports      = function (configuration) {
-    var router      = express.Router(),
+	var router      = express.Router(),
 	authenticate    = require('azure-mobile-apps/src/express/middleware/authenticate')(configuration),
 	authorize       = require('azure-mobile-apps/src/express/middleware/authorize');
 
 	var completeError = function(err, res) {
-	    if (err) {
-	        console.error(err);
-	        if (res) res.sendStatus(500);
-	    }
+		if (err) {
+			console.error(err);
+			if (res) res.sendStatus(500);
+		}
 	};
 
-    router.post('/', authenticate, (req, res, next) => {
+	router.post('/', authenticate, (req, res, next) => {
 		console.log('in POST /acceptinvitation');
-    	// var userSid = req.azureMobile.user.id;
-    	var userSid = req.body.userSid;
-    	console.log(userSid);
-    	var voucher = req.body.voucher;
-    	console.log(voucher);
+		// var userSid = req.azureMobile.user.id;
+		var userSid = req.body.userSid;
+		console.log(userSid);
+		var voucher = req.body.voucher;
+		console.log(voucher);
 		res.json( {
 			"links": [
 				{ "rel": "userSid", "method": "voucher", "href": "/ok" }
@@ -52,8 +52,9 @@ module.exports      = function (configuration) {
 			.catch(function(err) {
 				next(err);
 			});
-    	next();
-    });
+		});
+		next();
+	});
 
-    return router;
+	return router;
 };
