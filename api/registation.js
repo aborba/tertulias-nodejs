cb3ac43fdb4ba5c332ea9d8e24b027922d654386
Antonio Borba da Silva <abs@ggl.pt>
2016-08-22 00:11:43 +0100

    wip save

--- api/registation.js
+++ api/registation.js
@@ -101,7 +101,7 @@ module.exports      = function (configuration) {
 						}
 					});
 					client.invokeApi('/tertulias/voucherinfo/' + voucher, { body: null, method: "get"})
-					.done(function(results) {
+					.done(function(results) {
 						// var name = results.result.tertulias[0].name || "no name";
 						var name = "no name";
 						// var subject = results.result.tertulias[0].subject || "no subject";
