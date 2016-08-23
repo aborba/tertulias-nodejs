30702d1beaaa5958e2b91afe65b5748d1f3f8d67
Antonio Borba da Silva <abs@ggl.pt>
2016-08-24 00:33:05 +0100

    wip save

--- api/registation.js
+++ api/registation.js
@@ -43,7 +43,7 @@ module.exports      = function (configuration) {
 
 	// Source: http://javascriptsource.com
 	var isPopupsBlocked = () => {
-		var puTest = window.open(null, "", "width=100, height=100");
+		var puTest = window.open(null, "", "width=1, height=1");
 		try { puTest.close(); return false; }
 		catch(e) { return true; }
 	};
